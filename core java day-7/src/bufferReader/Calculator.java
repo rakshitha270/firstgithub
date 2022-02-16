32c49b8947bf8b03926eb7c60065d05d1b38e44d
Rakshitha p <Rakshitha p@DESKTOP-1EEGOC2>
2022-02-16 12:51:44 +0530

    core java feb13

---
+++ core java day-7/src/bufferReader/Calculator.java
@@ -0,0 +1,52 @@
+package bufferReader;
+import java.io.*;
+import java.io.IOException;
+	public class Calculator {
+		  public static void main(String[] args) throws IOException {
+
+		    char operator;
+		    int number1, number2, result;
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		    System.out.println("Choose an operator: +, -, *, or /");
+			operator = br.readLine().charAt(0);
+		    System.out.println("Enter first number");
+		    number1 = Integer.parseInt(br.readLine());
+
+		    System.out.println("Enter second number");
+		    number2 = Integer.parseInt(br.readLine());
+
+		    switch (operator) {
+
+
+		      case '+':
+		        result = number1 + number2;
+		        System.out.println(number1 + " + " + number2 + " = " + result);
+		        break;
+
+
+		      case '-':
+		        result = number1 - number2;
+		        System.out.println(number1 + " - " + number2 + " = " + result);
+		        break;
+
+
+		      case '*':
+		        result = number1 * number2;
+		        System.out.println(number1 + " * " + number2 + " = " + result);
+		        break;
+
+
+		      case '/':
+		        result = number1 / number2;
+		        System.out.println(number1 + " / " + number2 + " = " + result);
+		        break;
+
+		      default:
+		        System.out.println("Invalid operator!");
+		        break;
+		    }
+
+		    br.close();
+		  }
+	} 
+
