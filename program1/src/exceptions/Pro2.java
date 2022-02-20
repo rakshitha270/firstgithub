553847ef102a3b088afab923a4c2e65f04e08a42
Rakshitha p <Rakshitha p@DESKTOP-1EEGOC2>
2022-02-20 21:22:46 +0530

    core java day11

---
+++ program1/src/exceptions/Pro2.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class Pro2 {
+		public static void main (String[] args) {
+			int a=10;
+			try {
+			for(int i=3;i>=0;i--)
+			{
+			     System.out.println(a/i);  
+			   }	
+	}
+			catch(ArithmeticException e) {}
+	}
+}
+
+
---
+++ program1/src/exceptions/pro6.java
@@ -0,0 +1,14 @@
+package exceptions;
+import java.util.*;
+class pro6 {
+	public static void findFile(){
+
+	       File newFile = new File("test.txt");
+	    FileInputStream stream = new FileInputStream(newFile);
+	  }
+
+	  public static void main(String[] args) {
+	    System.out.println("Print the exception");
+	  }
+	}
+
