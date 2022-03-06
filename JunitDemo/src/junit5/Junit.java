ea8e2f6590a57b6a59cf51c15f335aee2581df2d
Rakshitha p <Rakshitha p@DESKTOP-1EEGOC2>
2022-03-06 20:19:22 +0530

    Junit

--- JunitDemo/src/junit5/Junit.java
+++ JunitDemo/src/junit5/Junit.java
@@ -1,5 +1,48 @@
 package junit5;
 
-public class Junit {
 
-}
+	import org.junit.After;
+	import org.junit.AfterClass;
+
+	import org.junit.Before;
+	import org.junit.BeforeClass;
+
+	
+	import org.junit.Test;
+
+	public class Junit {
+		
+	   
+	   @BeforeClass
+	   public static void beforeClass() {
+	      System.out.println("in before class");
+	   }
+
+	   
+	   public static void  afterClass() {
+	      System.out.println("in after class");
+	   }
+
+	   
+	   @Before
+	   public void before() {
+	      System.out.println("in before");
+	   }
+		
+	   
+	   @After
+	   public void after() {
+	      System.out.println("in after");
+	   }
+		
+	   
+	   @Test
+	   public void test() {
+	      System.out.println("in test");
+	   }
+		
+	   
+	   
+	   
+	}
+
